23b14e701c160bf66c74c80ad3c8a55d402c61cd
Tyler <tsmith328@gatech.edu>
2015-01-28 20:31:39 -0500

    Revert "Loading it into Eclipse"
    This reverts commit 3f84f4c4be407769f7c81cee95dbc0c09053e6b9.

--- app/src/main/gen/edu/gatech/cs2340/willcode4money/shoppingwithfriends/app/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.gatech.cs2340.willcode4money.shoppingwithfriends.app;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
