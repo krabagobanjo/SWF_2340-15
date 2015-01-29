6e175350f612fa763ca31ef1d626cbf8c9475b81
Kyle Rabago-Banjo <krabagobanjo@outlook.com>
2015-01-28 21:41:27 -0500

    This shit fucked up
    redo!

---
+++ app/build/generated/source/buildConfig/debug/edu/gatech/cs2340/willcode4money/shoppingwithfriends/app/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.gatech.cs2340.willcode4money.shoppingwithfriends.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.gatech.cs2340.willcode4money.shoppingwithfriends.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "edu.gatech.cs2340.willcode4money.shoppingwithfriends.app";
+}
