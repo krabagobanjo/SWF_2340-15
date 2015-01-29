3f84f4c4be407769f7c81cee95dbc0c09053e6b9
Slugpuppy <jake@slugpuppy.com>
2015-01-28 20:25:14 -0500

    Loading it into Eclipse

---
+++ app/src/main/gen/edu/gatech/cs2340/willcode4money/shoppingwithfriends/app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.gatech.cs2340.willcode4money.shoppingwithfriends.app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
