23853486d95a50156591a59b40aa8df7cc85d374
Tyler <tsmith328@gatech.edu>
2015-01-30 13:17:15 -0500

    Renamed activities
    Renamed some activities to make the app name correct.

--- app/src/main/java/edu/gatech/cs2340/willcode4money/shoppingwithfriends/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/willcode4money/shoppingwithfriends/MainActivity.java
@@ -4,9 +4,7 @@ package edu.gatech.cs2340.willcode4money.shoppingwithfriends;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 
 import willcode4money.cs2340.gatech.edu.shoppingwithfriends.R;
 
@@ -15,18 +13,8 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final Activity ACTIVITY = this;
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-//        Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                startActivity(new Intent(ACTIVITY, Login.class));
-//            }
-//        },5000); //adding five sec delay
     }
 
     public void buttonPressed(View view) {
