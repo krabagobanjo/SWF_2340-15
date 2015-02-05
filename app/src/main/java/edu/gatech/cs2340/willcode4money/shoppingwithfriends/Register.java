dd1c190ce88331a0b359f4b713b9002a8513642d
Jake Williams <jake@slugpuppy.com>
2015-02-05 13:09:38 -0500

    added a confirmation field and blocked the password fields

--- app/src/main/java/edu/gatech/cs2340/willcode4money/shoppingwithfriends/Register.java
+++ app/src/main/java/edu/gatech/cs2340/willcode4money/shoppingwithfriends/Register.java
@@ -19,6 +19,7 @@ public class Register extends Activity {
     // UI references.
     private EditText usernameEditText;
     private EditText passwordEditText;
+    private EditText passwordConfirmEditText;
     private TextView infoTextView;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,29 +28,37 @@ public class Register extends Activity {
 
         usernameEditText = (EditText) findViewById(R.id.usernameET);
         passwordEditText = (EditText) findViewById(R.id.passwordET);
+        passwordConfirmEditText = (EditText) findViewById(R.id.passwordConfirmET);
         infoTextView = (TextView) findViewById(R.id.infoTV);
     }
 
     public void register(View view) {
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
+        String passwordConfirm = passwordConfirmEditText.getText().toString();
 
         Map<String, String> tempUsers = ((MyApplication) this.getApplication()).getUsers();
-        if (!tempUsers.containsKey(username)) {
-            tempUsers.put(username, password);
-            ((MyApplication) this.getApplication()).setUsers(tempUsers);
-            infoTextView.setText("Success");
+        if (password.equals(passwordConfirm)) {
+            if (!tempUsers.containsKey(username)) {
+                tempUsers.put(username, password);
+                ((MyApplication) this.getApplication()).setUsers(tempUsers);
+                infoTextView.setText("Success");
 
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    finish();
-                }
-            }, 1000);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                }, 1000);
+            } else {
+                infoTextView.setText("That username already exists");
+                usernameEditText.setText("");
+                passwordEditText.setText("");
+            }
         } else {
-            infoTextView.setText("That username already exists");
-            usernameEditText.setText("");
+            infoTextView.setText("There was a mismatch in the passwords");
             passwordEditText.setText("");
+            passwordConfirmEditText.setText("");
         }
     }
 
