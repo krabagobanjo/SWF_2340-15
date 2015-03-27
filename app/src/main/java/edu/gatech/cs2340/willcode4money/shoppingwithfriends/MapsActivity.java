573e17b8e6ac3aa258cf6a00c03b31e672065fe1
jkim3086 <jkim3086@gatech.edu>
2015-03-27 00:35:24 -0700

    google map

--- app/src/main/java/edu/gatech/cs2340/willcode4money/shoppingwithfriends/MapsActivity.java
+++ app/src/main/java/edu/gatech/cs2340/willcode4money/shoppingwithfriends/MapsActivity.java
@@ -1,14 +1,21 @@
  package edu.gatech.cs2340.willcode4money.shoppingwithfriends;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
-
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -16,48 +23,38 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity {
+ public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener
+{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-    private double latitude;
-    private double longitude;
-    private List<Address> geocodeMatches = null;
-
-    private View tempV = null;
+    private double target_latitude, target_longitude, currentLatitude, currentLongitude;
     private int tempId = -1;
+    private int index = 0;
+    private View tempV = null;
+    private List<Address> Addresses = null;
+    //private List<Address> BoundAddress = null;
+    //public static final String TAG = MapsActivity.class.getSimpleName();
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private String loCation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        CheckBox hybrid_mode, satellite_mode, terrain_mode, none_mode;
         Intent intent = getIntent();
-        CheckBox hybrid_mode;
-        CheckBox satellite_mode;
-        CheckBox terrain_mode;
-        CheckBox none_mode;
-        String location;
-        location = (String) intent.getSerializableExtra("Location_info");
-        //location = "North AvNW Atlanta GA 30332";
+
+        loCation = (String) intent.getSerializableExtra("Location_info");
         hybrid_mode = (CheckBox) findViewById(R.id.hybrid);
         satellite_mode = (CheckBox) findViewById(R.id.satellite);
         terrain_mode = (CheckBox) findViewById(R.id.terrain);
         none_mode = (CheckBox) findViewById(R.id.none);
-
-        try {
-            geocodeMatches = new Geocoder(this).getFromLocationName(location, 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if ((geocodeMatches == null) || (geocodeMatches.isEmpty())) {
-            Toast.makeText(MapsActivity.this, "No matches were found or there is no backend service!",
-                    Toast.LENGTH_SHORT).show();
-        } else {
-            latitude = geocodeMatches.get(0).getLatitude();
-            longitude = geocodeMatches.get(0).getLongitude();
-        }
 
         setUpMapIfNeeded();
 
@@ -65,12 +62,29 @@ public class MapsActivity extends FragmentActivity {
         satellite_mode.setOnClickListener(CheckBoxOnClickListener);
         terrain_mode.setOnClickListener(CheckBoxOnClickListener);
         none_mode.setOnClickListener(CheckBoxOnClickListener);
-    }
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        mLocationRequest = LocationRequest.create().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(10 * 1000).setFastestInterval(1000);
+    }
+    /**
+     * Resume the map activity
+     */
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+        mGoogleApiClient.connect();
+    }
+    /**
+     * Pause the connection
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
     }
 
     /**
@@ -108,19 +122,26 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
+        LatLng latLng = new LatLng(target_latitude, target_longitude);
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        LatLng lat = new LatLng(latitude, longitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(lat));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("HERE"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(target_latitude, target_longitude)).title("HERE"));
     }
 
+    /**
+     * Creating listener for checkboxes and call setCheckB
+     */
     private final CheckBox.OnClickListener CheckBoxOnClickListener = new CheckBox.OnClickListener() {
         public void onClick(View v) {
             setCheckB(v);
         }
     };
-
+    /**
+     * Setting the check boxes to change map types
+     *
+     * @param v view of the app
+     */
     private void setCheckB(View v) {
         boolean checked = ((CheckBox) v).isChecked();
 
@@ -157,5 +178,129 @@ public class MapsActivity extends FragmentActivity {
         }
         tempId = v.getId();
         tempV = v;
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} connection is suspended.
+     */
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+    /**
+     * This is called when location is changed.
+     * Then call handleNewLocation method
+     *
+     * @param location changed location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    /**
+     * Getting the current place's latitude and longitude
+     * then find addresses of sales locations
+     *
+     * @param location location of current location
+     */
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+        List<Address> BoundAddress;
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        try{
+            BoundAddress = new Geocoder(this, Locale.getDefault()).getFromLocation(currentLatitude, currentLongitude, 1);
+            String stateName = BoundAddress.get(0).getAddressLine(1);
+            StringBuilder bound = new StringBuilder(" "+stateName).replace(1,7,"");
+            loCation = loCation + bound;
+            Addresses = new Geocoder(this, Locale.getDefault()).getFromLocationName(loCation,100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if ((Addresses == null) || (Addresses.isEmpty())) {
+            Toast.makeText(MapsActivity.this, "Could not find the place!", Toast.LENGTH_SHORT).show();
+        } else {
+            if(Addresses.size() > 1) {index = Dist_Calc(Addresses);}
+            target_latitude = Addresses.get(index).getLatitude();
+            target_longitude = Addresses.get(index).getLongitude();
+        }
+        setUpMap();
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is connected.
+     */
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else {
+            handleNewLocation(location);
+        }
+    }
+    /**
+     * Called when {@code mGoogleApiClient} is trying to connect but failed.
+     * Handle {@code result.getResolution()} if there is a resolution
+     * available.
+     *
+     * @param connectionResult result of connection of mGoogleApiClient
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    /**
+     * Calculate distances between candidate places and current location.
+     * Then choose a closest place.
+     *
+     * @param list A list of candidate addresses
+     * @return pos An index of the closest place
+     */
+    private int Dist_Calc(List<Address> list) {
+        double dif_lat, dif_long, temp_dist, short_dist;
+        int pos = 0;
+        int i;
+        double stan_lat = currentLatitude;
+        double stan_long = currentLongitude;
+        short_dist = 0;
+
+        for(i = 0; i < list.size(); i++) {
+            dif_lat = stan_lat - (list.get(i).getLatitude());
+            dif_long = stan_long - (list.get(i).getLongitude());
+            temp_dist = Math.sqrt(dif_lat*dif_lat + dif_long*dif_long);
+            if(temp_dist < short_dist) {
+                short_dist = temp_dist;
+                pos = i;
+            }
+        }
+        return pos;
     }
 }
